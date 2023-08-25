882597d672f38caf80ce170b70093cb9362964fd
Vesna Tan <tanvezi@gmail.com>
2023-08-25 09:37:49 -0400

    fix a11y issue#5

--- src/chainlit/frontend/src/components/organisms/header.tsx
+++ src/chainlit/frontend/src/components/organisms/header.tsx
@@ -1,5 +1,6 @@
+import { grey } from 'palette';
 import { useRef, useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 
 import MenuIcon from '@mui/icons-material/Menu';
@@ -9,10 +10,12 @@ import {
   Button,
   IconButton,
   Menu,
+  MenuItem,
   Stack,
   Toolbar,
   useTheme
 } from '@mui/material';
+import { Theme } from '@mui/material';
 import useMediaQuery from '@mui/material/useMediaQuery';
 
 import RegularButton from 'components/atoms/buttons/button';
@@ -27,6 +30,11 @@ interface INavItem {
   label: string;
 }
 
+interface IMenuItem {
+  to: string;
+  label: string;
+  tabIndex: number;
+}
 function ActiveNavItem({ to, label }: INavItem) {
   return (
     <RegularButton component={Link} to={to} key={to}>
@@ -35,25 +43,51 @@ function ActiveNavItem({ to, label }: INavItem) {
   );
 }
 
+const styleOverrides = {
+  inactive: {
+    textTransform: 'none',
+    color: 'text.secondary',
+    '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
+      outline: '2px solid crimson',
+      background: 'transparent'
+    }
+  },
+  active: {
+    textTransform: 'none',
+    color: (theme: Theme) =>
+      theme.palette.mode === 'dark'
+        ? 'text.primary'
+        : theme.palette.primary.main,
+    background: (theme: Theme) =>
+      theme.palette.mode === 'dark' ? grey[700] : theme.palette.primary.light,
+    '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
+      outline: '2px solid crimson',
+      background: (theme: Theme) =>
+        theme.palette.mode === 'dark' ? grey[700] : theme.palette.primary.light
+    }
+  }
+};
+
 function NavItem({ to, label }: INavItem) {
   return (
-    <Button
-      component={Link}
-      to={to}
-      key={to}
-      sx={{
-        textTransform: 'none',
-        color: 'text.secondary',
-        '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
-          outline: '2px solid crimson',
-          background: 'transparent'
-        }
-      }}
-    >
+    <Button component={Link} to={to} key={to} sx={styleOverrides.inactive}>
       {label}
     </Button>
   );
 }
+
+const ActiveMenuItem = ({ to, label, tabIndex }: IMenuItem) => (
+  <MenuItem
+    component={Link}
+    key={to}
+    to={to}
+    tabIndex={tabIndex}
+    autoFocus={true}
+    sx={styleOverrides.active}
+  >
+    {label}
+  </MenuItem>
+);
 
 interface NavProps {
   hasDb?: boolean;
@@ -131,7 +165,22 @@ function Nav({ hasDb, hasReadme }: NavProps) {
           anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
           transformOrigin={{ vertical: 'bottom', horizontal: 'left' }}
         >
-          {nav}
+          {tabs.map((t, index) => {
+            const active = location.pathname === t.to;
+            return active ? (
+              <ActiveMenuItem {...t} tabIndex={index} />
+            ) : (
+              <MenuItem
+                sx={styleOverrides.inactive}
+                key={t.to}
+                component={Link}
+                to={t.to}
+                tabIndex={index}
+              >
+                {t.label}
+              </MenuItem>
+            );
+          })}
         </Menu>
       </>
     );
