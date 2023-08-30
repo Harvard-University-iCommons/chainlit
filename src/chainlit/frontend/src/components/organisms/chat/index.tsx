c6459a477533e60e92dd8f6be94bed42bbf5aab8
Vesna Tan <tanvezi@gmail.com>
2023-08-29 22:03:36 -0400

    add sidebar

--- src/chainlit/frontend/src/components/organisms/chat/index.tsx
+++ src/chainlit/frontend/src/components/organisms/chat/index.tsx
@@ -5,6 +5,7 @@ import { v4 as uuidv4 } from 'uuid';
 import { Alert, Box } from '@mui/material';
 
 import Head from 'components/Head';
+import Sidebar from 'components/Sidebar';
 import SideView from 'components/atoms/element/sideView';
 import ErrorBoundary from 'components/atoms/errorBoundary';
 import TaskList from 'components/molecules/tasklist';
@@ -105,6 +106,7 @@ const Chat = () => {
   return (
     <Box display="flex" width="100%" height="0" flexGrow={1}>
       <Head title="Chat" description="Chat" />
+      <Sidebar />
       <Playground />
       <ChatSettingsModal />
       <TaskList tasklist={tasklist} isMobile={false} />
