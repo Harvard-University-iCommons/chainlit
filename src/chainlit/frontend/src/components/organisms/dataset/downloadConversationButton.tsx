2d48bbc40f29de4c840bafda7279b949784840bc
Colin Murtaugh <cmurtaugh@gmail.com>
2023-09-01 17:18:29 -0400

    fix download button

--- src/chainlit/frontend/src/components/organisms/dataset/downloadConversationButton.tsx
+++ src/chainlit/frontend/src/components/organisms/dataset/downloadConversationButton.tsx
@@ -15,14 +15,29 @@ export default function DownloadConversationButton({ conversationId }: Props) {
       // to={`/conversations/${conversationId}/text`}
       size="small"
       color="primary"
-      onClick={async () =>
+      onClick={async () => {
         await fetch(`/conversations/${conversationId}/text`, {
           headers: {
             'content-type': 'text/plain'
           },
           method: 'GET'
         })
-      }
+          .then((response) => {
+            return response.blob();
+          })
+          .then((blob) => {
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = `conversation_${conversationId}.txt`;
+            document.body.appendChild(a); // append the element to the dom
+            a.click();
+            a.remove(); // afterwards, remove the element
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      }}
     >
       <DownloadIcon aria-label="download conversation" />
     </IconButton>
