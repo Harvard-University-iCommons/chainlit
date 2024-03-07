27d95cb39686771528c6f16bf6e027fdce6c8708
Haydn <haydn_ady@harvard.edu>
2024-03-07 17:04:19 -0500

    WIP

--- src/chainlit/hello.py
+++ src/chainlit/hello.py
@@ -1,5 +1,6 @@
 # This is a simple example of a chainlit app.
 
+from chainlit.session import Session
 from fastapi import Request
 from fastapi.responses import JSONResponse
 
@@ -23,7 +24,25 @@ async def get_version(request: Request):
 
 
 @app.get("/projects/{session_id}")
-async def get_projects(request: Request, session_id: str):
-    print(f"----> Session ID: {session_id}")
-    payload = {"projects": ["p4"]}
-    return JSONResponse(content=payload)
+async def get_user_projects(request: Request, session_id: str) -> JSONResponse:
+    """
+    Get AI Sandboxes a user has access to.
+    """
+    # we need to get a hold of the websocket context in order to push messages
+    # newer version of chainlit does this via a dedicated websocket session:
+    #     ws_session = WebsocketSession.get_by_id(session_id=session_id)
+    #     init_ws_context(ws_session)
+    # but since we don't have that atm, do it ourselves with the regular session object
+    print(f"SESSION_ID ============================================================================================================================  {type(session_id)} {session_id}")
+    session = Session.get_by_id(session_id=session_id)
+    print(f"SESSION ============================================================================================================================ {session}")
+
+    try:
+        user_infos = await session.auth_client.get_user_infos()
+        print(f"user_infos ============================================================================================================================ {user_infos}")
+        if session:
+            return JSONResponse(content={"projects": ["Success"]})
+        # return JSONResponse(content={"projects": user_infos["projects"]})
+    except Exception as e:
+        print(f"---------> Failed to get user_infos: {e}")
+        raise
